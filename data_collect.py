89e6ad15b3e80ba7723a978c34a306323532575d
Calebkillom <Calebkillom@gmail.com>
2024-03-10 10:55:50 +0300

    task 2a) improvements

--- data_collect.py
+++ data_collect.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+import os
 """
 Fetches historical stock data for Exxon Mobil Corporation (stock symbol: XOM)
 from Yahoo Finance from 2018 to 2024. The data is saved to a CSV file
@@ -13,10 +14,17 @@ end_date = "2024-03-10"
 """ Fetch historical data """
 data = yf.download(stock_symbol, start=start_date, end=end_date)
 
-""" Format the dates for the filename """
-filename = f"{stock_symbol}_historical_data_{start_date}_{end_date}.csv"
+if not data.empty:
+    """ Format the dates for the filename """
+    filename = f"{stock_symbol}_historical_data_{start_date}_{end_date}.csv"
 
-""" Print the data """
-print(data)
+    """ Save the data to a CSV file """
+    data.to_csv(filename)
 
-data.to_csv(filename)
+    if os.path.isfile(filename):
+        print(f"Data was successfully saved to '{filename}'")
+    else:
+        print("Failed to save data to CSV")
+
+else:
+    print("No data available for the specified time period")
