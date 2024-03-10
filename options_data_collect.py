880c294983ba687a42e6a9ab3230fac1899b05e4
calebkillom <calebkillom@gmail.com>
2024-03-11 00:51:44 +0300

    task 2b) edits

--- options_data_collect.py
+++ options_data_collect.py
@@ -1,4 +1,6 @@
+#!/usr/bin/python3
 from bs4 import BeautifulSoup
+from selenium import webdriver
 import requests
 
 base_url = "https://www.marketwatch.com/investing/stock/xom/options"
@@ -6,7 +8,16 @@ query_string = (
     "?mod=mw_quote_tab"
 )
 
+
 URL = base_url + query_string
+
+driver = webdriver.Firefox()
+driver.get(URL)
+driver.implicitly_wait(10)
+driver.implicitly_wait(10)
+
+page_content = driver.page_source
+driver.quit()
 
 page = requests.get(URL)
 
@@ -16,7 +27,7 @@ else:
     print("Failed to fetch page content. Status code:", page.status_code)
     exit()
 
-soup = BeautifulSoup(page.content, "html.parser")
+soup = BeautifulSoup(page_content, "html.parser")
 
 """ Find all table bodies with class "table__body" """
 table_bodies = soup.find_all("tbody", class_="table__body")
