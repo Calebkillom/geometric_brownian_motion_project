acbc231911b044f746709a1750f253580eebe706
Calebkillom <Calebkillom@gmail.com>
2024-03-10 10:46:33 +0300

    task 2a) completed

--- data_collect.py
+++ data_collect.py
@@ -1,17 +1,22 @@
-import requests
-from bs4 import BeautifulSoup
+import yfinance as yf
+"""
+Fetches historical stock data for Exxon Mobil Corporation (stock symbol: XOM)
+from Yahoo Finance from 2018 to 2024. The data is saved to a CSV file
+with a filename that includes the stock symbol and the time period.
+"""
 
-base_url = "https://finance.yahoo.com/quote/XOM/history"
-query_string = (
-    "?period1=1520553600"
-    "&period2=1709942400"
-    "&interval=1d"
-    "&filter=history"
-    "&frequency=1d"
-    "&includeAdjustedClose=true"
-)
-URL = base_url + query_string
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, "html.parser")
+""" Specify the stock symbol and time period """
+stock_symbol = "XOM"
+start_date = "2018-03-09"
+end_date = "2024-03-10"
 
-stocks_info = []
+""" Fetch historical data """
+data = yf.download(stock_symbol, start=start_date, end=end_date)
+
+""" Format the dates for the filename """
+filename = f"{stock_symbol}_historical_data_{start_date}_{end_date}.csv"
+
+""" Print the data """
+print(data)
+
+data.to_csv(filename)
