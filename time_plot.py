158610a6bb89bb8441c57a5daa3bb45eee915514
Calebkillom <Calebkillom@gmail.com>
2024-03-13 10:32:43 +0300

    timeplot question 3b)

---
+++ time_plot.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+""" Read csv File and convert it to a dataframe """
+file_path = "XOM_historical_data_2018-03-09_2024-03-10.csv"
+historical_data = pd.read_csv(file_path)
+
+if not historical_data.empty:
+    print("csv file read successfully")
+    print(historical_data.head())
+else:
+    print("An error occurred!!!")
+
+""" Create a new column of Price which is the daily adjusted closing stock """
+historical_data["Price"] = historical_data["Adj Close"]
+print(historical_data.head())
+
+""" Convert the Date column to datetime Object """
+historical_data["Date"] = pd.to_datetime(
+    historical_data["Date"], format='%d/%m/%Y')
+
+""" Set the 'Date' column as the index """
+historical_data.set_index("Date", inplace=True)
+
+""" Plot the "Price" column """
+historical_data["Price"].plot(figsize=(10, 6), color="blue")
+
+""" Set the title and labels """
+plt.title('Time Plot of Price')
+plt.xlabel('Date')
+plt.ylabel('Price')
+
+""" Show the plot """
+plt.grid(True)
+plt.show()
